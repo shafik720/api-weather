05805c485275c949b1d413d92933efb9c7478e6f
shafik720 <shafirkasel5@gmail.com>
2022-09-06 18:15:39 +0600

    sdfsf

---
+++ app.js
@@ -0,0 +1,6 @@
+let inputField = document.getElementById('inputField');
+
+document.getElementById('showBtn').addEventListener('click', function(){
+    let inputFields = inputField.value;
+    console.log(inputFields);
+})
